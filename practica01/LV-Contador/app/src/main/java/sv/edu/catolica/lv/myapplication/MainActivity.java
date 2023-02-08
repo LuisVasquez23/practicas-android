9864c4a74ade7ea03a99bb1dabdb2508fc6b9c36
Luis Vasquez <vluis5457@gmail.com>
2023-02-07 18:08:16 -0600

    Practica01-Terminado

--- practica01/LV-Contador/app/src/main/java/sv/edu/catolica/lv/myapplication/MainActivity.java
+++ practica01/LV-Contador/app/src/main/java/sv/edu/catolica/lv/myapplication/MainActivity.java
@@ -3,12 +3,36 @@ package sv.edu.catolica.lv.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Obtener las variables
+    private TextView mensaje;
+    private Button btnClick;
+
+    private int contador;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Asignar variables
+        mensaje = findViewById(R.id.mensaje);
+        btnClick = findViewById(R.id.btn_click);
+
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                contador++;
+                mensaje.setText("Click numero" + contador);
+            }
+        });
     }
+
+
 }
